fb585d85cadfcac94b690699886412d65c71109b
Macharia <machariaelvys@gmail.com>
2024-07-26 12:39:59 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+function divide () {
+   let a = 2000
+   let b = 100
+   return a/b
+}
+ let square = () => {
+    let x =2
+    return x * x
+ }
+let add = (a, b) => {
+     a = 3
+     b = 4
+    return a + b
+}
